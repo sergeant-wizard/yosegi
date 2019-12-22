363b77c7b3fd9ddbe2168d8aacd8f3808cafddaf
Ryo Miyajima <sergeant.wizard@gmail.com>
2019-12-21 18:43:24 -0800

    csv support

---
+++ yosegi/io/csv_io.py
@@ -0,0 +1,22 @@
+import pathlib
+
+import pandas
+
+import yosegi
+
+
+class CsvIO:
+    @staticmethod
+    def save(
+        data: 'yosegi.Data',
+        path: pathlib.Path,
+    ) -> None:
+        data.to_dataframe().to_csv(path)
+
+    @staticmethod
+    def load(
+        path: pathlib.Path,
+    ) -> 'yosegi.Data':
+        return yosegi.Data.from_dataframe(
+            df=pandas.read_csv(path, index_col=0),
+        )
--- yosegi/io/formats.py
+++ yosegi/io/formats.py
@@ -1,5 +1,6 @@
 import enum
 
+from .csv_io import CsvIO
 from .joblib_io import JoblibIO
 from .parquet_io import ParquetIO
 
@@ -7,10 +8,12 @@ from .parquet_io import ParquetIO
 class Formats(enum.Enum):
     joblib = 1
     parquet = 2
+    csv = 3
 
     @property
     def formatter(self):
         return {
             Formats.joblib: JoblibIO,
             Formats.parquet: ParquetIO,
+            Formats.csv: CsvIO,
         }[self]
